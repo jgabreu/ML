a68fc4f0e47f5aed7603e14eb4ce93ac5aecc0ec
Joõeshámuitos <joaogoncaloabreu@gmail.com>
2019-12-06 22:30:17 +0100

    Introducing train_test_split function

--- Chapter1_Introduction.py
+++ Chapter1_Introduction.py
@@ -4,8 +4,9 @@ import matplotlib
 import scipy as sp
 import mglearn
 import IPython
-# import sklearn
+import sklearn
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 
 # print("Python version: {}".format(sys.version))
@@ -27,3 +28,13 @@ iris_dataset = load_iris()
 
 #print("Shape of data: {}".format(iris_dataset['target']))
 
+#train_test_split randomly mixes the data and splits it into two groups: the training data containing 75% of the cases and the testing data containing 25% of the cases
+
+X_train, X_test, y_train, y_test = train_test_split(
+        iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+print("X_train shape: {}".format(X_train.shape))
+print("y_train shape: {}".format(y_train.shape))
+
+print("X_test shape: {}".format(X_test.shape))
+print("y_test shape: {}".format(y_test.shape))
